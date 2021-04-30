ed4ee0279c665514d00d935dcb787f7c67ae4346
David Mauriello <dmauriello82@gmail.com>
2021-04-30 16:46:46 -0400

    ag-grid-angular added, styling still needs work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FixedTable';
+  columnDefs = [
+      {headerName: 'Id', field: "id"},
+      {headerName: 'Name', field: "name"},
+      {headerName: "email", field: "email"}
+  ];
+
+  rowData = [
+    {id: 1, name: "David", email: "david@david.com"},
+    {id: 2, name: "Edward", email: "ed@edd.eddy"},
+    {id: 3, name: "Francis", email: "Rand@cis.fr"}
+  ]
 }
